21e05e3379409e0e6bf1fa6249b45a2042e17e04
Berkay MIZRAK <mizrakby@gmail.com>
2023-03-29 16:01:25 +0300

    refactor: define apps with path

--- resume/celery.py
+++ resume/celery.py
@@ -1,7 +1,6 @@
 import os
 from celery import Celery
 import django
-from django.conf import settings
 
 # set the default Django settings module for the 'celery' program. Change `resume` with your project name.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume.settings')
@@ -12,5 +11,17 @@ app = Celery('resume')
 
 app.config_from_object('django.conf:settings')
 
+
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+# if you are using app names directly, you can use this: (user)
+# from django.conf import settings
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+# or if you are using app names as dotted paths, you can use this: (user.apps.UserConfig)
+# from django.apps import apps
+# app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
+
+# if you are using Celery 4.x you can use:
+app.autodiscover_tasks()
+
