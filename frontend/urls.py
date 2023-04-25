2e9e491e1a8516f70d1b9a99ea9708691ac66ea3
Berkay MIZRAK <mizrakby@gmail.com>
2023-04-25 10:56:39 +0300

    refactor: change url

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,5 +3,5 @@ from frontend import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('21_mayis_dugun/', views.invitation, name='invitation'),
+    path('21_mayis_nikah/', views.invitation, name='invitation'),
 ]
